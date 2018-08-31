9e03b2da5dc383f28c2e5505cb16d41dcec8beba
jacobplaster <jacob.plaster@youview.com>
2018-08-31 13:55:19 +0100

    Expose utils modules

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const GrpcClient = require("./client/grpc");
 const HttpClient = require("./client/http");
+const Utils      = require("./utils")
 
 module.exports = {
   GrpcClient,
   HttpClient,
+  Utils
 };
---
+++ src/utils/index.js
@@ -0,0 +1,13 @@
+const account     = require("./account");
+const address     = require("./address");
+const base64      = require("./base64");
+const bytes       = require("./bytes");
+const transaction = require("./transaction");
+
+module.exports = {
+  account,
+  address,
+  base64,
+  bytes,
+  transaction
+};
